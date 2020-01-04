dd98f830288b81cd7b9c04e2b778e915e7a6915e
Wills Grave <will@keydigitalconsulting.com>
2020-01-04 09:24:23 -0500

    Added form edits and new names for inputs and button functions.

--- client/src/components/Form/index.js
+++ client/src/components/Form/index.js
@@ -20,14 +20,14 @@ export function TextArea(props) {
 
 export function FormBtn(props) {
   return (
-    <button {...props} style={{ float: "right", marginBottom: 10 }} className="btn btn-success">
+    <button {...props} style={{ float: "center", marginBottom: 10 }} className="btn btn-success">
       {props.children}
     </button>
   );
 }
 
-// Sign up Form & Button
-export function SignUpInput(props) {
+// Signup Input & Form Button
+export function SignupInput(props) {
   return (
     <div className="form-group">
       <input className="form-control" {...props} />
@@ -37,7 +37,24 @@ export function SignUpInput(props) {
 
 export function SignUpBtn(props) {
   return (
-    <button {...props} style={{ float: "right", marginBottom: 10 }} className="btn btn-success">
+    <button {...props} style={{ float: "center", marginBottom: 10 }} className="btn btn-success btn-block btn-lg col-10">
+      {props.children}
+    </button>
+  );
+}
+
+// Login Input & Form Button
+export function LoginInput(props) {
+  return (
+    <div className="form-group">
+      <input className="form-control" {...props} />
+    </div>
+  );
+}
+
+export function LoginBtn(props) {
+  return (
+    <button {...props} style={{ float: "center", marginBottom: 10 }} className="btn btn-success btn-block btn-lg col-10">
       {props.children}
     </button>
   );
