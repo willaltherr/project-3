e7438cdea466e2330e1a04ab0abcaf9c7051f68a
Wills Grave <will@keydigitalconsulting.com>
2020-01-04 13:15:08 -0500

    Connecting to Mongo.

--- Config/keys.js
+++ Config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-  mongoURI: "mongodb+srv://waltherr:<Will4339>@project3-qwgo7.mongodb.net/test?retryWrites=true&w=majority" 
+  mongoURI: "mongodb+srv://waltherr:Jeff4339@project3-qwgo7.mongodb.net/test?retryWrites=true&w=majority" 
 };
